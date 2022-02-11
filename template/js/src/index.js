1c74fc6d4ad667eed4659ff3ccf50af775310d48
n-f9 <ndferguson@protonmail.com>
2022-02-11 23:41:02 +0000

    bug fix

--- template/js/src/index.js
+++ template/js/src/index.js
@@ -1,18 +1,21 @@
 import 'dotenv/config'
 import path from 'path'
 import { QuackJS, QuackJSUtils } from '@n-f9/quack.js'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const Quack = new QuackJS(process.env.TOKEN, {}) 
 
-const files = QuackJSUtils.GetFiles('./out/modules')
+const files = QuackJSUtils.GetFiles('./src/modules')
 
 const getModules = async () => {
 	for (const file of files) {
-		const execute = (await import(path.join('./', file))).default
+		const execute = (await import(path.join(__dirname, '../', file))).default
 		await execute(Quack)
 	}
 }
 
-getModules().then(() => {
-	Quack.Start(Quack)
-})
+await getModules()
+await Quack.Start(Quack)
